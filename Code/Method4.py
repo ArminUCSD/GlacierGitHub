0d89042876f9216cfd90d4cea2591d2062f682bf
aih <aih@tabulaw.com>
2016-08-30 10:56:52 -0700

    Back to temporal=0

--- Code/Method4.py
+++ Code/Method4.py
@@ -45,5 +45,5 @@ def estimateTerminus(path,glacier,arcVector,timeline,ipTimeSeries,gm,invert,dist
 	direc = path
 	if not os.path.exists(direc): os.makedirs(direc)
 	terminus = r_tp(glacier = glacier, obs = obs, ss = arcV, tt = timlin, sSmooth=smoothItem, theta0=theta0, meas = grndmeas, plot=ri.TRUE, direc = direc, linefit = 0,
-		temporal = 2, invert = invert, distPerYear = distPerYear)
+		temporal = 0, invert = invert, distPerYear = distPerYear)
 	return terminus
