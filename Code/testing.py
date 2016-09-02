1c9b00aaf4e3aabde4c36b5d802b0cbda23a0dfa
aih <aih@tabulaw.com>
2016-09-02 14:50:40 -0700

    Debugging

--- Code/testing.py
+++ Code/testing.py
@@ -5,7 +5,12 @@ def partial():
     landsatPath = pathjoin(path,'Data',GlacierName,'Landsat')
     DEMfile=DEMFILE2
     pathVectors, timeline, landsatFiles, terminus = analyze(path, GlacierName, DEMfile)
-
+    print("pathVectors")
+    print(pathVectors)
+    print("timeline")
+    print(timeline)
+    print("terminus")
+    print(terminus)
 
 if __name__ == '__main__':
     partial()
